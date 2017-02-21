6dd7872605b7272cfc8630f5fb79b9b1c0c149ed
keco185 <keco185@me.com>
2017-02-21 16:23:16 -0500

    Adds relative angle of target

--- src/vision/VisionResults.java
+++ src/vision/VisionResults.java
@@ -2,16 +2,17 @@ package vision;
 
 //Object that stores results of the vision calculations
 public class VisionResults {
-	public double relativeAngleX, relativeAngleY, inchesX, inchesY, inchesZ;
+	public double relativeAngleX, relativeAngleY, inchesX, inchesY, inchesZ, targetRotation;
 	public boolean targetFound = false;
 	
 	//Initialized this way when target was found
-	public VisionResults(double relativeAngleX, double relativeAngleY, double inchesX, double inchesY, double inchesZ) {
+	public VisionResults(double relativeAngleX, double relativeAngleY, double inchesX, double inchesY, double inchesZ, double targetRotation) {
 		this.relativeAngleX = relativeAngleX; //The angle of target along the x-y plane relative to robot orientation
 		this.relativeAngleY = relativeAngleY; //The angle of the target along the y-z plane relative to robot orientation
 		this.inchesX = inchesX; //The offset in the x axis of the target relative to the robot
 		this.inchesY = inchesY; //The offset in the y axis (height) of the target relative to the robot
 		this.inchesZ = inchesZ; //The distance from the robot to the target
+		this.targetRotation = targetRotation; //The horizontal rotation of the target relative to the robot
 		targetFound = true; //True if vision calculations successfully found the target
 	}
 	
--- src/vision/calculators/GearCalculator.java
+++ src/vision/calculators/GearCalculator.java
@@ -12,28 +12,44 @@ public class GearCalculator {
 	private static final double TAPE_WIDTH = 2; //Width of the tape (inches)
 	public static VisionResults run(ArrayList<Contour> contours, CameraSettings camSettings) {
 		ArrayList<Double> distances = new ArrayList<Double>();
-		distances.add(distanceFromHeight(contours, camSettings));
+		double[] results = distanceFromHeight(contours, camSettings);
+		distances.add(results[0]);
 		distances.add(distanceFromWidth(contours, camSettings));
 		distances.add(distanceFromSpacing(contours, camSettings));
 		double avgDistance = (distances.get(0) + distances.get(1) + distances.get(2)) / 3.0;
 		double angleX = offsetXAngleFromCenters(contours, camSettings);
 		double offsetX = Math.tan(angleX) * avgDistance;
 		//TODO: Properly look at different distance values to error correct
-		//TODO: Look at camera position and orientation when performing calculations
 		
-		return new VisionResults(angleX, 0, offsetX, 0, avgDistance);
+		return new VisionResults(angleX + camSettings.angleX, 0, offsetX + camSettings.posX, 0, avgDistance + camSettings.posY, results[1]);
 	}
 	
 	//Calculates distance to target based on height of the tape
-	private static double distanceFromHeight(ArrayList<Contour> contours, CameraSettings camSettings) {
+	private static double[] distanceFromHeight(ArrayList<Contour> contours, CameraSettings camSettings) {
 		double[] results = new double[2];
 		
-		for (int i = 0; i < 2; i++) {
-			double height = contours.get(i).height;
-			double angle = camSettings.radPerPixel * height;
-			results[i] = TAPE_HEIGHT / (2.0 * Math.tan(angle/2.0));
-		}
-		return (results[0] + results[1]) / 2.0;
+		Contour leftCntr = contours.get(0).centerX < contours.get(1).centerX ? contours.get(0) : contours.get(1);
+		Contour rightCntr = contours.get(0).centerX > contours.get(1).centerX ? contours.get(0) : contours.get(1);
+
+		double height = leftCntr.height;
+		double angle = camSettings.radPerPixel * height;
+		results[0] = TAPE_HEIGHT / (2.0 * Math.tan(angle/2.0));
+		
+		height = rightCntr.height;
+		angle = camSettings.radPerPixel * height;
+		results[1] = TAPE_HEIGHT / (2.0 * Math.tan(angle/2.0));
+		
+		double width = Math.abs(contours.get(0).centerX - contours.get(1).centerX);
+		angle = camSettings.radPerPixel * width;
+		
+		
+		//4 ways to get relative rotation of target and robot using difference in heights of tapes
+		double targetAngleLoS1 = Math.asin(Math.sin(angle) * results[0] / TAPE_SPACING) + angle / 2.0 - Math.PI / 2.0;
+		double targetAngleLoS2 = Math.PI / 2.0 - angle / 2.0 - Math.asin(Math.sin(angle) * results[1] / TAPE_SPACING);
+		double targetAngleLOC1 = Math.acos((Math.pow(TAPE_SPACING, 2)+Math.pow(results[1], 2)-Math.pow(results[0], 2))/(2.0 * TAPE_SPACING * results[1])) + angle / 2.0 - Math.PI / 2.0;
+		double targetAngleLOC2 = Math.PI / 2.0 - angle / 2.0 - Math.acos((Math.pow(TAPE_SPACING, 2)+Math.pow(results[0], 2)-Math.pow(results[1], 2))/(2.0 * TAPE_SPACING * results[0]));
+
+		return new double[] {(results[0] + results[1]) / 2.0, (targetAngleLoS1 + targetAngleLoS2 + targetAngleLOC1 + targetAngleLOC2) / 4.0};
 	}
 	
 	//Calculates distance to target based on the width of the tape
